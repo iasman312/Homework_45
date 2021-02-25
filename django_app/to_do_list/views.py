4d6b93c54267c0faa5835dea02638d38582a15d6
Asman <iasman312@gmail.com>
2021-02-25 15:28:12 +0600

    Создали все необходимые вьюшки

--- django_app/to_do_list/views.py
+++ django_app/to_do_list/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task
+
+
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'index.html', context={'articles': tasks})
+
+
+def task_view(request):
+    task_id = request.GET.get('id')
+    task = Task.objects.get(id=task_id)
+    return render(request, 'task_view.html', context={'article': task})
+
+
+def task_create_view(request):
+    if request.method == "GET":
+        return render(request, 'article_create.html')
+    elif request.method == "POST":
+        title = request.POST.get("title")
+        status = request.POST.get("status")
+        up_to = request.POST.get("up_to")
+
+        task = Task.objects.create(
+            title=title,
+            status=status,
+            up_to=up_to
+        )
+
+        return render(request, 'task_view.html', context={'task': task})
+
