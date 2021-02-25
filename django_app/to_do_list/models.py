35ba6b9016be61242ed37addffafc0d18a7b9d99
Asman <iasman312@gmail.com>
2021-02-25 15:28:29 +0600

    Создали все необходимые модели

--- django_app/to_do_list/models.py
+++ django_app/to_do_list/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
+                      ('done', 'Сделано')]
+    title = models.CharField(max_length=120, null=False, blank=False)
+    status = models.CharField(max_length=150, null=False, blank=False,
+                              default="new")
+    up_to = models.DateField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return f'{self.id}. {self.title}: {self.status}'
