0cf558895020bd20f35a32573e10fbbe97c2a2f9
Asman <iasman312@gmail.com>
2021-02-25 16:29:18 +0600

    Изменил логику во вьюшках

--- django_app/to_do_list/views.py
+++ django_app/to_do_list/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 
-from .models import Task
+from .models import Task, status_choices
 
 
 def index_view(request):
     tasks = Task.objects.all()
-    return render(request, 'index.html', context={'articles': tasks})
+    return render(request, 'index.html', context={'tasks': tasks})
 
 
 def task_view(request):
     task_id = request.GET.get('id')
     task = Task.objects.get(id=task_id)
-    return render(request, 'task_view.html', context={'article': task})
+    return render(request, 'task_view.html', context={'task': task})
 
 
 def task_create_view(request):
     if request.method == "GET":
-        return render(request, 'article_create.html')
+        context = {'status_choices': status_choices}
+        return render(request, 'task_create.html', context)
     elif request.method == "POST":
         title = request.POST.get("title")
         status = request.POST.get("status")
