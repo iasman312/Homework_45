e76741e9bbd3ce76d6788df022cbcff72ef2a61a
Asman <iasman312@gmail.com>
2021-02-27 16:01:28 +0600

    Исправили ошибки, которые были, добавили навигацию и добавили title всем шаблонам

--- django_app/to_do_list/urls.py
+++ django_app/to_do_list/urls.py
@@ -4,6 +4,6 @@ from .views import index_view, task_view, task_create_view, delete_task
 urlpatterns = [
     path('', index_view, name='task-list'),
     path('tasks/add/', task_create_view, name='task-add'),
-    path('task/<int:fk>/', task_view, name='task-view'),
-    path('delete_task/<int:fk>/', delete_task, name='task-delete'),
+    path('task/<int:pk>/', task_view, name='task-view'),
+    path('delete_task/<int:pk>/', delete_task, name='task-delete'),
 ]
--- django_app/to_do_list/views.py
+++ django_app/to_do_list/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 
 from .models import Task, status_choices
 
