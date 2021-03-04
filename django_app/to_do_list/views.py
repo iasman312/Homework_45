68c3d9943cd25fd3c5b36a381b65da87ac02cee5
Asman <iasman312@gmail.com>
2021-03-04 17:01:32 +0600

    Переделали проект, использовали встроенные формы джанго

--- django_app/to_do_list/views.py
+++ django_app/to_do_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Task, status_choices
+from .forms import TaskForm
 
 
 def index_view(request):
@@ -15,40 +16,51 @@ def task_view(request, pk):
 
 def task_create_view(request):
     if request.method == "GET":
-        context = {'status_choices': status_choices}
+        form = TaskForm()
+        context = {'status_choices': status_choices, 'form': form}
         return render(request, 'task_create.html', context)
     elif request.method == "POST":
-        title = request.POST.get("title")
-        status = request.POST.get("status")
-        up_to = request.POST.get("up_to")
-        if not up_to:
-            up_to = None
-        description = request.POST.get("description")
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = Task(
+                title=form.cleaned_data.get('title'),
+                status=form.cleaned_data.get('status'),
+                up_to=form.cleaned_data.get('up_to'),
+                description=form.cleaned_data.get('description')
+            )
+            task.save()
+            return redirect('task-view', pk=task.id)
 
-        task = Task.objects.create(
-            title=title,
-            status=status,
-            up_to=up_to,
-            description=description,
-        )
-        return redirect('task-view', pk=task.id)
+        return render(request, 'task_create.html', context={'form': form})
 
 
 def task_update_view(request, pk):
     task = get_object_or_404(Task, id=pk)
 
     if request.method == 'GET':
-        return render(request, 'task_update.html',
-                      context={'task': task, 'status_choices': status_choices})
+        form = TaskForm(initial={
+            'title': task.title,
+            'status': task.status,
+            'up_to': task.up_to,
+            'description': task.description
+        })
+        return render(request, 'task_update.html', context={'form': form,
+                                                               'task': task})
+
     elif request.method == 'POST':
-        task.title = request.POST.get("title")
-        task.status = request.POST.get("status")
-        task.up_to = request.POST.get("up_to")
-        if not task.up_to:
-            task.up_to = None
-        task.description = request.POST.get("description")
-        task.save()
-        return redirect('task-view', pk=task.id)
+        form = TaskForm(
+            data=request.POST)
+        if form.is_valid():
+            task.title = form.cleaned_data.get("title")
+            task.status = form.cleaned_data.get("status")
+            task.up_to = form.cleaned_data.get("up_to")
+            task.description = form.cleaned_data.get("description")
+            task.save()
+            return redirect('article-view',
+                            pk=task.id)
+
+        return render(request, 'task_create.html', context={'form': form,
+                                                               'task': task})
 
 
 def task_delete_view(request, pk):
