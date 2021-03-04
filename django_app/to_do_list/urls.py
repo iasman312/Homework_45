d16fd23a256f71203d301f8ed83659767d0fba7c
Asman <iasman312@gmail.com>
2021-03-04 12:56:15 +0600

    Удалил delete_task

--- django_app/to_do_list/urls.py
+++ django_app/to_do_list/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import index_view, task_view, task_create_view, delete_task
+from .views import index_view, task_view, task_create_view
 
 urlpatterns = [
     path('', index_view, name='task-list'),
     path('tasks/add/', task_create_view, name='task-add'),
     path('task/<int:pk>/', task_view, name='task-view'),
-    path('delete_task/<int:pk>/', delete_task, name='task-delete'),
 ]
--- django_app/to_do_list/views.py
+++ django_app/to_do_list/views.py
@@ -13,13 +13,6 @@ def task_view(request, pk):
     return render(request, 'task_view.html', context={'task': task})
 
 
-def delete_task(request, pk):
-    task = get_object_or_404(Task, id=pk)
-    task.delete()
-    tasks = Task.objects.all()
-    return render(request, 'index.html', context={'tasks': tasks})
-
-
 def task_create_view(request):
     if request.method == "GET":
         context = {'status_choices': status_choices}
